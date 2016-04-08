142d29316fe159db931894cfb7098121c4b381a8
Hemal Shah <hjshah25@gmail.com>
2016-04-07 22:51:47 -0400

    Modified to remove loops

--- fastlane/lib/fastlane/lane_manager.rb
+++ fastlane/lib/fastlane/lane_manager.rb
@@ -108,25 +108,25 @@ module Fastlane
         UI.error "It looks like you don't have any lanes to run just yet. Check out how to get started here: https://github.com/fastlane/fastlane 🚀"
         exit
       end
+      
+      rows = []
+      available.each_with_index do |lane, index|
+        rows << [index + 1, lane.last.pretty_name, lane.last.description.first]
+      end
+
       loop do
         UI.message "Welcome to fastlane! Here's what your app is setup to do:"
-        rows = []
-        available.each_with_index do |lane, index|
-          rows << [index + 1, lane.last.pretty_name, lane.last.description.first]
-        end
 
-        unless rows.empty?
-          rows << [rows.size + 1, "cancel", "No selection!"]
+        rows << [rows.size + 1, "cancel", "No selection!"]
 
-          table = Terminal::Table.new(
-            title: "Available lanes to run",
-            headings: ['Number', 'Lane Name', 'Description'],
-            rows: rows
-          )
-          puts table
+        table = Terminal::Table.new(
+          title: "Available lanes to run",
+          headings: ['Number', 'Lane Name', 'Description'],
+          rows: rows
+        )
+        puts table
 
-          i = UI.input "Which number would you like run?"
-        end
+        i = UI.input "Which number would you like run?"
 
         i = i.to_i - 1
         if i >= 0 && available[i]
